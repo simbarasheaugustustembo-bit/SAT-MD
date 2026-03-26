77eade001db6f3f1f6e884c9aecf7ccde26d90e1
Simbarashe Augustus Tembo <simbarasheaugustustembo@gmail.com>
2026-03-26 22:22:30 +0200

    Delete plugins/menu.js

--- plugins/menu.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    command: ["menu"],
-
-    async run(sock, from) {
-        await sock.sendMessage(from, {
-            text: "🔥 SAT MD MENU 🔥\n\n.menu\n.coinflip\n.ai\n.play"
-        });
-    }
-};
